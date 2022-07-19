0d4ca8555df0f3a0afbeda04b33d935aefed0014
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-19 09:52:44 +0200

    server folder and basic requirements

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const http = require('http');
+
+const app = express();
+
+const clientPath = `${__dirname}/../client`;
+
+app.use(express.static(clientPath));
+
+const server = http.createServer(app);
+
+server.listen(8080, () =>{
+    console.log("server running on "+ 8080);
+});
+
+const io = require('socket.io')(server);
+let counter = 0
+io.on('connection', (socket) => {
+    console.log(counter+' someone connected');
+    counter++;
+    socket.on('sendToAll', (message) =>{
+        io.emit("displayMessage", (message));
+    });
+
+});
