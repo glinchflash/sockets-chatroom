498e8e8936374c5b46f51dfd92cf50a9f1999bfd
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-22 09:38:47 +0200

    added functionallity of online users list

--- server/server.js
+++ server/server.js
@@ -15,7 +15,10 @@ server.listen(8080, () =>{
 });
 //require socket.io and make our io variable the entry point of all clients/sockets connecting to our server
 const io = require('socket.io')(server);
+// variable for array of users
 let usernames = [];
+// variable for user in server to log out
+let serverUser;
 //counter for the amount of people connected
 let counter = 0;
 io.on('connection', (socket) => {
@@ -28,7 +31,21 @@ io.on('connection', (socket) => {
         socket.emit("displayMessage", (message)); //only send back to the original socket
     });
     socket.on('sendToList', (username)=>{ //get username from socket to send back to the same socket
+        serverUser = username;
         usernames.push(username) // push username received from client into usernames array
         io.emit("displayList", (usernames)) //return the usernames array to the client
     });
+    socket.on("disconnecting", () => { //get the socket that was disconnected
+        counter--; // reduces counter by 1
+        console.log("user has left: " + counter + " are connected"); //log in console (terminal)
+        usernames = removeItemOnce(usernames , serverUser); //remove user from array
+        io.emit("displayRemovedUsers", usernames); //push back to clients
+    });
 });
+function removeItemOnce(arr, value) {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+        arr.splice(index, 1);
+    }
+    return arr;
+}
